57fb42d2b2a03230ddee57e1caf16b519b1a8a80
Luiz Carlos Fischer <lc.fischer@hotmail.com>
2019-02-07 11:02:43 -0200

    Update DesafioPages.java

--- src/test/java/pages/DesafioPages.java
+++ src/test/java/pages/DesafioPages.java
@@ -99,60 +99,55 @@ public class DesafioPages {
 		
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		
-//      Olá, tudo bem? Infelizmente não consegui fazer a validação dos campos da nota fiscal, tentei por xpath, css selector, 
-//		e também tentei montar meu proprio xpath utilizando um plug in do chrome chamado xpath helper. 
-//		Estou muito curioso para saber como realizar essa validação. 
-//		Abaixo estão as minhas tentativas.
+		String validarCustomerDtls = driver.findElement(By.xpath("/h4[text()='Rendezvous Hotel']/following-sibling::div[1]")).getText();
+		Assert.assertEquals("JOHNY SMITHR2, AVENUE DU MAROC123456", validarCustomerDtls);
 		
-//		String validarCustomerDtls = driver.findElement(By.xpath("/h4[text()='Rendezvous Hotel']/following-sibling::div[1]")).getText();
-//		Assert.assertEquals("JOHNY SMITHR2, AVENUE DU MAROC123456", validarCustomerDtls);
+		WebElement validarNomeHotel = driver.findElement(By.cssSelector(".mt-5:nth-child(2)"));
+		Assert.assertEquals("Rendezvous Hotel", validarNomeHotel);
 		
-//		WebElement validarNomeHotel = driver.findElement(By.cssSelector(".mt-5:nth-child(2)"));
-//		Assert.assertEquals("Rendezvous Hotel", validarNomeHotel);
+		String validarDataInvoice = driver.findElement(By.xpath("/html/body/section/div/ul/li")).getText();
+		Assert.assertEquals("Invoice Date: 14/01/2018", validarDataInvoice);
 		
-//		String validarDataInvoice = driver.findElement(By.xpath("/html/body/section/div/ul/li")).getText();
-//		Assert.assertEquals("Invoice Date: 14/01/2018", validarDataInvoice);
-		
-//		String validarDueDate = driver.findElement(By.xpath("/html/body/section/div/ul/li[2]")).getText();
-//		Assert.assertEquals("15/01/2018", validarDueDate);
-//
-//		String validarInvoiceNumber = driver.findElement(By.xpath("/html/body/section/div/h6")).getText();
-//		Assert.assertEquals("Invoice #110 details", validarInvoiceNumber);
-//
-//		String validarBookingCode = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[1]/td[2]"))
-//				.getText();
-//		Assert.assertEquals("0875", validarBookingCode);
-//
-//		String validarRoom = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[2]/td[2]"))
-//				.getText();
-//		Assert.assertEquals("Superior Double", validarRoom);
-//
-//		String validarCheckIn = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[5]/td[2]"))
-//				.getText();
-//		Assert.assertEquals("14/01/2018", validarCheckIn);
-//
-//		String validarCheckOut = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[6]/td[2]"))
-//				.getText();
-//		Assert.assertEquals("15/01/2018", validarCheckOut);
-//
-//		String validarStayCount = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[3]/td[2]"))
-//				.getText();
-//		Assert.assertEquals("1", validarStayCount);
-//
-//		String validarStayAmount = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[4]/td[2]"))
-//				.getText();
-//		Assert.assertEquals("$150", validarStayAmount);
-//
-//		String validarDepositNow = driver.findElement(By.xpath("/html/body/section/div/table[2]/tbody/tr/td[1]"))
-//				.getText();
-//		Assert.assertEquals("USD $20.90", validarDepositNow);
-//
-//		String validarTaxVat = driver.findElement(By.xpath("/html/body/section/div/table[2]/tbody/tr/td[2]")).getText();
-//		Assert.assertEquals("USD $19", validarTaxVat);
-//
-//		String validarTotalAmount = driver.findElement(By.xpath("/html/body/section/div/table[2]/tbody/tr/td[3]"))
-//				.getText();
-//		Assert.assertEquals("USD $209", validarTotalAmount);
+		String validarDueDate = driver.findElement(By.xpath("/html/body/section/div/ul/li[2]")).getText();
+		Assert.assertEquals("15/01/2018", validarDueDate);
+
+		String validarInvoiceNumber = driver.findElement(By.xpath("/html/body/section/div/h6")).getText();
+		Assert.assertEquals("Invoice #110 details", validarInvoiceNumber);
+
+		String validarBookingCode = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[1]/td[2]"))
+				.getText();
+		Assert.assertEquals("0875", validarBookingCode);
+
+		String validarRoom = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[2]/td[2]"))
+				.getText();
+		Assert.assertEquals("Superior Double", validarRoom);
+
+		String validarCheckIn = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[5]/td[2]"))
+				.getText();
+		Assert.assertEquals("14/01/2018", validarCheckIn);
+
+		String validarCheckOut = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[6]/td[2]"))
+				.getText();
+		Assert.assertEquals("15/01/2018", validarCheckOut);
+
+		String validarStayCount = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[3]/td[2]"))
+				.getText();
+		Assert.assertEquals("1", validarStayCount);
+
+		String validarStayAmount = driver.findElement(By.xpath("/html/body/section/div/table[1]/tbody/tr[4]/td[2]"))
+				.getText();
+		Assert.assertEquals("$150", validarStayAmount);
+
+		String validarDepositNow = driver.findElement(By.xpath("/html/body/section/div/table[2]/tbody/tr/td[1]"))
+				.getText();
+		Assert.assertEquals("USD $20.90", validarDepositNow);
+
+		String validarTaxVat = driver.findElement(By.xpath("/html/body/section/div/table[2]/tbody/tr/td[2]")).getText();
+		Assert.assertEquals("USD $19", validarTaxVat);
+
+		String validarTotalAmount = driver.findElement(By.xpath("/html/body/section/div/table[2]/tbody/tr/td[3]"))
+				.getText();
+		Assert.assertEquals("USD $209", validarTotalAmount);
 
 	}
 
